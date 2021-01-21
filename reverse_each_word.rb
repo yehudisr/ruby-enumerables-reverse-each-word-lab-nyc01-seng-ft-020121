94fe5369bd1ba25a9f5bc0b619e5e710603c54e5
yehudis raitman <yehudisr@gmail.com>
2021-01-21 03:19:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,7 +12,7 @@ require "pry"
 def reverse_each_word(string)
 string_to_array = string.split (" ")
 string_to_array.collect do |element| 
-   puts element.reverse
+  element.reverse
   binding.pry
 end  
 element.reverse.join(" ")
