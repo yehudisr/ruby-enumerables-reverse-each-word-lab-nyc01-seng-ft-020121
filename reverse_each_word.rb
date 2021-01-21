22e832e833bd430dd115ae595d6333f5929692e2
yehudis raitman <yehudisr@gmail.com>
2021-01-21 02:06:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,20 +1,18 @@
 require "pry"
 
-def reverse_each_word(sentence)
-sentence = sentence.split (" ")
-new_array = []
-sentence.each do |element| 
- new_array << element.reverse
-end  
-new_array.join(" ")
-end  
+#def reverse_each_word(sentence)
+#sentence = sentence.split (" ")
+#new_array = []
+#sentence.each do |element| 
+# new_array << element.reverse
+#end  
+#new_array.join(" ")
+#end  
 
-def reverse_each_word(sentence)
-sentence_to_array = sentence.split (" ")
-new_array = []
-sentence_to_array.collect do |element| 
- new_array << element.reverse
+def reverse_each_word(string)
+string_to_array = string.split (" ")
+string_to_array.collect do |element| 
+ element.reverse
 end  
-new_array.join(" ")
 end  
 
